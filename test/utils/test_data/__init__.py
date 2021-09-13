0f46d415d3e358001633b5afda182c9bb502f84d
Jaspal Puri <j-puri.github@outlook.com>
2021-09-13 22:19:35 +0100

    Fixing typo

--- test/utils/test_data/__init__.py
+++ test/utils/test_data/__init__.py
@@ -2,7 +2,7 @@ from test import test_project_root
 
 
 class TestFiles:
-    VOCALINK_VALACDOS = 'vocalink_4998_valacdos.txt'
+    VOCALINK_VALACDOS = 'vocalink_4997_valacdos.txt'
     VOCALINK_SCSUBTAB = 'vocalink_1517_scsubtab.txt'
     VOCALINK_NOT_FOUND = 'vocalink_not_valid.txt'
 
